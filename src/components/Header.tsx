37af5c9d5808bf04eb4216cc7431f8b2dba5e903
Fcojpv <penav.francisco@gmail.com>
2025-09-23 21:59:44 -0300

    cambio en header y meta:og

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,7 +60,7 @@ const Header = () => {
           <div className="flex items-center space-x-2">
             <Book className="h-8 w-8 text-celestial-500" />
             <span className="text-xl font-poppins font-bold text-gradient-celestial">
-              Libros de Luz y Esperanza
+              Libros Amparo y Fortaleza
             </span>
           </div>
           
